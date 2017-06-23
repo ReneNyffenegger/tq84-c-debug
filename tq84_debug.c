348c763c4ce508b68b861ff260adbffd1bf55eff
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-06-23 10:03:13 +0200

    Adding initial files

---
+++ tq84_debug.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+
+#include "tq84_debug.h"
+
+FILE* f_debug = NULL;
+
+int   indent;
+
+
+void tq84_debug_out(char* fmt, va_list ap) {
+//va_list ap; va_start(ap, fmt);
+  vfprintf(f_debug, fmt, ap);
+
+  fprintf(f_debug, "\n");
+}
+
+void tq84_debug_indent_() {
+  int i;
+  for (i=0; i<indent*2; i++) {
+    fprintf(f_debug, " ");
+  }
+}
+
+void tq84_debug_open() {
+  time_t t;
+  struct tm tm;
+  char file_name[200];
+
+  t=time(NULL);
+  tm = *localtime(&t);
+  sprintf(file_name, "c:\\temp\\tq84_vim_debug_%4d-%02d_%02d_%02d.%02d.%02d", tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+  f_debug = fopen(file_name, "a");
+}
+
+
+void tq84_debug_indent(char* fmt, ...) {
+
+  va_list ap; va_start(ap, fmt);
+ 
+  tq84_debug_indent_();
+  tq84_debug_out(fmt, ap);
+
+  tq84_debug_out("{\n", NULL);
+
+  indent++;
+}
+void tq84_debug_dedent(/*char* fmt, ...*/) {
+
+  tq84_debug_indent_();
+//va_list ap; va_start(ap, fmt);
+//tq84_debug_out(fmt, ap);
+
+  tq84_debug_out("}\n", NULL);
+
+  indent--;
+}
+void tq84_debug(char* fmt, ...) {
+  va_list ap; va_start(ap, fmt);
+  tq84_debug_out(fmt, ap);
+
+  tq84_debug_indent_();
+
+  tq84_debug_out("\n", NULL);
+}
---
+++ tq84_debug.h
@@ -0,0 +1,5 @@
+void tq84_open();
+void tq84_debug_dedent(/*char* fmt, ...*/);
+void tq84_debug_indent(char* fmt, ...);
+void tq84_debug       (char* fmt, ...);
+//void tq84_debug_out(char* fmt, ...);
